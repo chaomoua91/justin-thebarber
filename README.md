24b7f62472f84973c732e7ec48509aa50dac9b88
Chao Moua <160983755+chaomoua91@users.noreply.github.com>
2024-08-14 19:19:38 -0500

    chore: Initialize React + Vite project for Justin's Barber Shop

---
+++ README.md
@@ -0,0 +1,5 @@
+# React + Vite
+
+This is my first real client.
+
+Commercial website for Justin's Barber Shop.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Justin's Barber Shop</h1>
+    </>
+  );
+}
+
+export default App;
